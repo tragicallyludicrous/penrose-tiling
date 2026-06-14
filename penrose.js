0529718d0e0a8c727ce187bb8a4f878dff7384fa
tragicallyludicrous <95321639+tragicallyludicrous@users.noreply.github.com>
2026-06-14 10:48:45 -0400

    progress on inherited classes

--- penrose.js
+++ penrose.js
@@ -1,36 +1,30 @@
-let shape;
+let dart;
+let kite;
 let PHI = (1 + Math.sqrt(5)) / 2; // The Golden Ratio constant
 
 function setup() {
   createCanvas(400, 400);
   angleMode(DEGREES);
-  shape = new Shape(100);
+  dart = new Dart(100, 0, 0, 0);
+  kite = new Kite(100, 0, 0, 0);
 }
 
 function draw() {
   background(210);
   translate(width / 2, height / 2);
-  shape.kite();
-  // shape.dart();
+  kite.show();
+  dart.show();
 }
 
 class Shape {
-  constructor(l, x = 0, y = 0) {
+  constructor(l, x = 0, y = 0, rot = 0) {
     this.kiteLength = l;
     this.h = l / (1 / tan(36) + 1 / tan(72));
     this.x = x;
     this.y = y;
-    this.rot = 0;
+    this.rot = rot;
   }
-  #drawArcs(x, y, l, a1, a2, rot, arc1, arc2) {
-    // Red Arc
-    push();
-    noFill();
-    stroke("red");
-    strokeWeight(2);
-    arc(x + l, y, a2 * 2, a2 * 2, (rot + arc2) % 360, (rot + arc2) % 360);
-    pop();
-
+  drawArcs(x, y, l, a1, a2, rot, arc1, arc2) {
     // Green Arc
     push();
     noFill();
@@ -38,53 +32,121 @@ class Shape {
     strokeWeight(2);
     arc(x, y, a1 * 2, a1 * 2, (rot - arc1) % 360, (rot + arc1) % 360);
     pop();
-  }
 
-  dart() {
-    let rot = this.rot + 180;
-    let l = this.kiteLength / PHI;
-    let x1 = l + this.x;
-    let y1 = this.y;
-    let x2 = this.x - this.h / tan(72);
-    let y2 = this.y - this.h;
-    let x3 = this.x;
-    let y3 = this.y;
-    let x4 = x2;
-    let y4 = this.y + this.h;
-    let a1 = l / PHI;
-    let a2 = l - a1;
-    let arc = 36;
-    // atan(this.h / l);
-
+    // Red Arc
     push();
-    stroke("black");
+    noFill();
+    stroke("red");
     strokeWeight(2);
-    quad(x1, y1, x2, y2, x3, y3, x4, y4);
-    this.#drawArcs(x1, y1, -l, a1, a2, rot, arc);
+    arc(
+      x + l,
+      y,
+      a2 * 2,
+      a2 * 2,
+      (rot + 180 - arc2) % 360,
+      (rot + 180 + arc2) % 360,
+    );
     pop();
   }
+}
 
-  kite() {
-    let l = this.kiteLength;
-    let x1 = this.x;
-    let y1 = this.y;
-    let x2 = this.h / tan(36) + this.x;
-    let y2 = this.y - this.h;
-    let x3 = l + this.x;
-    let y3 = this.y;
-    let x4 = x2;
-    let y4 = this.y + this.h;
-    let a1 = l / PHI;
-    let a2 = l - a1;
-    let arc1 = 36;
-    let arc2 = 72;
+class Dart extends Shape {
+  constructor(kiteLength = 40, x = 0, y = 0, rot = 0) {
+    super(kiteLength, x, y, rot);
+    this.x = x;
+    this.y = y;
+    this.rot = rot;
 
-    // Kite Body
+    this.rot = this.rot + 180;
+    this.l = this.kiteLength / PHI;
+    this.x1 = this.l + this.x;
+    this.y1 = this.y;
+    this.x2 = this.x - this.h / tan(72);
+    this.y2 = this.y - this.h;
+    this.x3 = this.x;
+    this.y3 = this.y;
+    this.x4 = this.x2;
+    this.y4 = this.y + this.h;
+    this.a1 = this.l / PHI;
+    this.a2 = this.l - this.a1;
+    this.arc1 = 36;
+    this.arc2 = 105;
+  }
+  show() {
     push();
     stroke("black");
     strokeWeight(2);
-    quad(x1, y1, x2, y2, x3, y3, x4, y4);
-    this.#drawArcs(x1, y1, l, a1, a2, arc1, arc2);
+    quad(
+      this.x1,
+      this.y1,
+      this.x2,
+      this.y2,
+      this.x3,
+      this.y3,
+      this.x4,
+      this.y4,
+    );
+    this.drawArcs(
+      this.x1,
+      this.y1,
+      -this.l,
+      this.a1,
+      this.a2,
+      this.rot,
+      this.arc1,
+      this.arc2,
+    );
+    pop();
+  }
+}
+
+class Kite extends Shape {
+  constructor(kiteLength = 40, x = 0, y = 0, rot = 0) {
+    super(kiteLength, x, y, rot);
+    this.l = this.kiteLength;
+    this.x = x;
+    this.y = y;
+    this.rot = rot;
+
+    this.rot = this.rot;
+    this.l = this.kiteLength;
+    this.x1 = this.x;
+    this.y1 = this.y;
+    this.x2 = this.h / tan(36) + this.x;
+    this.y2 = this.y + this.h;
+    this.x3 = this.l + this.x;
+    this.y3 = this.y;
+    this.x4 = this.x2;
+    this.y4 = this.y - this.h;
+    this.a1 = this.l / PHI;
+    this.a2 = this.l - this.a1;
+    this.arc1 = 36;
+    this.arc2 = 72;
+  }
+  show() {
+    push();
+    stroke("black");
+    strokeWeight(2);
+    quad(
+      this.x1,
+      this.y1,
+      this.x2,
+      this.y2,
+      this.x3,
+      this.y3,
+      this.x4,
+      this.y4,
+    );
+    this.drawArcs(
+      this.x1,
+      this.y1,
+      -this.l,
+      this.a1,
+      this.a2,
+      this.rot,
+      this.arc1,
+      this.arc2,
+    );
     pop();
   }
 }
